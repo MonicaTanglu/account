40d895f80b89f7df92bc95c202b21e99840e8ac1
tanglu <wasltds@163.com>
2021-06-01 09:42:53 +0800

    增加版本更新提示和重新加载

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -27,7 +27,19 @@ Page({
   },
 
   onLoad: function () {
-
+    const updateManager = wx.getUpdateManager()
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
     // let category = app.globalData.category
     // if(!category) {
     //   wx.setStorageSync('category', icons.icons)
